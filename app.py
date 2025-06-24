d2f67898cde4e33c7943d1e2a9b38f3bf29801ee
Mohan <84068862+Sm1321@users.noreply.github.com>
2025-06-24 23:16:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-import streamlit as st
-import os
+from src.langgraphagenticai.main import load_langgraph_agenticai_app
 
-
-from src.langgraph_agenticai.main import load_langgraph_agenticai_app
-
-
-if __name__ == "__main__":
+if __name__=="__main__":
     load_langgraph_agenticai_app()
