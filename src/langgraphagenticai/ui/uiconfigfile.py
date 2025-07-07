c0cf2713d24f65701fbd989d8b4f1cb545ceeba5
Sm1321 <siddulamohan1321@gmail.com>
2025-07-07 21:31:03 +0530

    updated the uiconfigfile.py

--- src/langgraphagenticai/ui/uiconfigfile.py
+++ src/langgraphagenticai/ui/uiconfigfile.py
@@ -1,7 +1,26 @@
 from configparser import ConfigParser
-
-
+# This class is used to parse and manage configuration files, typically in the INI format. 
+# These files are structured with sections (enclosed in square brackets []) 
+# and key-value pairs within those sections.
 
 
 class Config:
-    pass
+    def __init__(self,config_file = "./src/langgraphagenticai/ui/uiconfigfile.ini"):
+        self.config = ConfigParser()
+        self.config.read(config_file)
+
+    #We get the all Info from the uiconfigfile.ini file
+    def get_llm_options(self):
+        #it will retunr all the LLLM OPtions in the Deafult one
+        return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ") #Split by comma and space
+    
+    def get_use_case_options(self):
+        return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ")
+    
+    def get_groq_model_options(self):
+        #it will retun all the LLLM OPtions in the Deafult one
+        return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(", ") #Split by comma and space
+    
+    def get_page_title(self):
+        return self.config["DEFAULT"].get("PAGE_TITLE")
+    
